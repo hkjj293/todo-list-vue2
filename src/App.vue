8896ff9cb0a7e42807e4029d9b595ccd84ef6317
hkjj293 <chrishuihuihui@gmail.com>
2023-03-09 13:49:31 +0000

    resolve #2

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,11 @@ import TheWelcome from "./components/TheWelcome.vue";
 <template>
   <div id="app">
     <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="./assets/logo.svg"
-        width="125"
-        height="125"
-      />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
+      <title>Todo-list Vue2</title>
     </header>
 
     <main>
-      <TheWelcome />
+
     </main>
   </div>
 </template>
