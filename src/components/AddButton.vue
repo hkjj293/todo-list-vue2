b2f248cabefc9e1dd33e3c91416a8b88ad3d9fb4
hkjj293 <chrishuihuihui@gmail.com>
2023-03-10 15:44:53 +0000

    resolved #5

---
+++ src/components/AddButton.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+    data() {
+        return {
+            content: "",
+            done: false,
+            editMode: false,
+        }
+    },
+    mounted() {
+
+    },
+    emits: ['add-job'],
+    methods: {
+        onClick() {
+            this.$emit('add-job');
+        }
+    }
+}
+</script>
+
+<template>
+    <div v-if="done == false">
+        <button @click="onClick">+</button>
+    </div>
+</template>
+
+<style scoped></style>
