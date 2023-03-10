f2b99b195ea07c0d3afd33951d66499ae4de8610
hkjj293 <chrishuihuihui@gmail.com>
2023-03-10 15:45:27 +0000

    resolve #3

---
+++ src/components/JobList.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default {
+    data() {
+        return {
+            jobs: [],
+        }
+    },
+    mounted() {
+
+    },
+    props: ['userId'],
+    methods: {
+        onAddJob() {
+            this.jobs.push({
+                content: "",
+                done: false,
+                editMode: false,
+            })
+        },
+        onDeleteJob(id) {
+            this.jobs.splice(id, 1);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jobList">
+        <div v-for="(job, index) in jobs">
+            <Job :id=index :job=job @delete="onDeleteJob" />
+        </div>
+        <div>
+            <AddButton @add-job="onAddJob" />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
