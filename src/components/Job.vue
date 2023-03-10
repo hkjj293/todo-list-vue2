a8e5ae63a4a564779a3c3710fc0b35a387440ae8
hkjj293 <chrishuihuihui@gmail.com>
2023-03-10 15:45:34 +0000

    resolve #4

---
+++ src/components/Job.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+    data() {
+        return {
+            content: this.job.content,
+            done: this.job.done,
+            editMode: this.job.editMode,
+            myClass: 'job',
+
+        }
+    },
+    mounted() {
+
+    },
+    emits: ['delete'],
+    props: ['job', 'id'],
+    methods: {
+        onFocus() {
+            this.editMode = true;
+            this.$nextTick(() => {
+                this.$refs.textRef.focus();
+            })
+        },
+        onBlur() {
+            this.editMode = false;
+        },
+        onDelete() {
+            this.$emit('delete', this.id);
+        },
+        isInputShown() {
+            return !this.done && (this.editMode || this.content == "");
+        }
+    }
+}
+</script>
+
+<template>
+    <div :class="myClass" @keypress.enter="onBlur" tabindex="1">
+        <input style="margin-right: 5px;" type="checkbox" v-model="done" />
+        <input v-if="isInputShown()" @focus="onFocus" type="text" v-model="content" placeholder="What is the task?"
+            @blur="onBlur" ref="textRef">
+        <span v-else @focus="onFocus" tabindex="1">{{ content }}</span>
+        <button class="remove-button" @click="onDelete">-</button>
+    </div>
+</template>
+
+<style scoped>
+.remove-button {
+    justify-self: right;
+}
+
+.job {
+    padding: 0px;
+    min-width: 100px;
+    background-color: beige;
+    display: grid;
+    text-align: left;
+    grid-template-columns: 20px auto 50px;
+}
+
+.done {
+    background-color: gray;
+}
+</style>
